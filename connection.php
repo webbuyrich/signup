9bbfeeae6b5ccae10e169c76f6c0cb8fb5a0ae0a
Richard Peterson <webbuyrich@gmail.com>
2015-02-04 07:55:31 -0600

    updated connection
    change db connection files

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
-$conn = mysql_connect("db564106911.db.1and1.com", "dbo564106911", "signupdatabase1");
+$conn = mysql_connect("yourhost", "yourusername", "yourpassword");
 if (!$conn) {
     die('Could not connect: ' . mysql_error());
 }
-$db = mysql_select_db("db564106911", $conn);
+$db = mysql_select_db("yourdatabase", $conn);
 ?>
